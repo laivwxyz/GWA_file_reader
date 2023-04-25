5af117a75ce271879e91de4f91bd9e31cd6e7777
laivwxyz <delacruzlailanie2004@gmail.com>
2023-04-25 13:07:06 +0800

    create file named student_info.txt for reading

--- gwa_file_reader.py
+++ gwa_file_reader.py
@@ -5,11 +5,14 @@
 # Write a Python program that read a file containing the name of 20 students together with their GWA. 
 # The program will outputs the name of the student who got the highest GWA (including the GWA).
 
-
+def highest_gwa():
 # create file named students_info.txt for reading
-# initialize variables
-# read students_info.txt each line
-# split the line into the student's name and their GWA
-# convert the GWA to a float
-# check the students who have a highest GWA
-# print out the name of the student with the highest GWA
+    with open('student_info.txt', 'r') as student_info:
+        # initialize variables
+        # read students_info.txt each line
+            # split the line into the student's name and their GWA
+            # convert the GWA to a float
+            # check the students who have a highest GWA
+        # print out the name of the student with the highest GWA
+
+highest_gwa()
