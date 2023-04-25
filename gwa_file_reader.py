39abc04ffc67219b41380239e962ff1d9417bb97
laivwxyz <delacruzlailanie2004@gmail.com>
2023-04-25 12:49:19 +0800

    problem and pseudocode has been added

--- gwa_file_reader.py
+++ gwa_file_reader.py
@@ -1,3 +1,14 @@
 # Dela Cruz, Lailanie E. _ BSCPE 1-4
 # Assignment 4
 # Programming Exercises 2
+
+# Write a Python program that read a file containing the name of 20 students together with their GWA. 
+# The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# create file named students_info.txt for reading 
+        # initialize variables
+        # read students_info.txt each line
+            # split the line into the student's name and their GWA
+            # convert the GWA to a float
+            # check the students who have a highest GWA
+        # print out the name of the student with the highest GWA
